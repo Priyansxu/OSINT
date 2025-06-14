347236d3943bba1af31f6299230adf35b1a29180
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-06-14 17:00:52 +0530

    Update manifest.js

--- app/manifest.js
+++ app/manifest.js
@@ -2,16 +2,16 @@ export default function manifest() {
   return {
     name: 'OSINT Tools',
     short_name: 'OSINT Tools',
-    description: '.',
+    description: 'A curated collection of open source intelligence tools and resources',
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#000000',
     icons: [
       {
-        src: '',
+        src: './icon.svg',
         sizes: 'any',
-        type: '',
+        type: 'image/svg+xml',
       },
     ],
   }
