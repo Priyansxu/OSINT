abcc704b4674d58f0b80c211092253eb8b96468f
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-06-18 12:56:01 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { Inter, Montserrat } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata = {
